af123a757e48d13840f8d67bca8d6ebeea532309
rossconlin <ross.conlin@atos.net>
2022-08-24 13:54:51 +0100

    final change

--- src/main/java/com/qa/Project/CRUDqueries.java
+++ src/main/java/com/qa/Project/CRUDqueries.java
@@ -26,19 +26,21 @@ public class CRUDqueries {
 		}
 			}
 	
-	public void create(String customer_first_name, String customer_last_name, int number_of_patties, String sauce, String salad) {
+	public String create(String customer_first_name, String customer_last_name, int number_of_patties, String sauce, String salad) {
 		
 		String createstmt = "INSERT INTO burger_order(customer_first_name, customer_last_name, number_of_patties, sauce, salad) VALUES('"+customer_first_name+"', '"+customer_last_name+"', "+number_of_patties+",'"+sauce+"','"+salad+"');";
 		try {
 			stmt.executeUpdate(createstmt);
 			System.out.println("Create statement executed");
+			return createstmt;
 		}catch(SQLException e) {
 			System.out.println("bad query");
 			e.printStackTrace();
+			return null;
 			
 		}
 	}
-	public void read() {
+	public String read() {
 		String readStmt = "SELECT * FROM burger_order";
 		try {
 			rs = stmt.executeQuery(readStmt);
@@ -49,44 +51,52 @@ public class CRUDqueries {
 				System.out.println("number of patties: " + rs.getInt("number_of_patties"));
 				System.out.println("sauce: " + rs.getString("sauce"));
 				System.out.println("salad: " + rs.getString("salad"));
-			}
+				
+			} return readStmt;
 		}catch(SQLException e) {
 			System.out.println("bad query");
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
-	public void update(int id, String updateVal) {
+	public String update(int id, String updateVal) {
 		String updateStmt = "UPDATE burger_order SET customer_first_name + customer_last_name = '" + updateVal + "' WHERE id = " + id + ";";
 		try {
 			stmt.executeUpdate(updateStmt);
 			System.out.println("Update Statement executed");
+			return null;
 		} catch (SQLException e) {
 			System.out.println("Bad Query");
 			e.printStackTrace();
+			return null;
 	}
 	}
 	
-	public void delete(int id) {
+	public String delete(int id) {
 		String delStmt = "DELETE FROM burger_order WHERE id="+id+";";
 		try {
 			stmt.executeUpdate(delStmt);
 			System.out.println("Delete Statement executed");
+			return null;
 		} catch (SQLException e) {
 			System.out.println("Bad Query");
 			e.printStackTrace();
+			return null;
 		}
 	}
 	
 	
 	
-	public void CloseConn() {
+	public String CloseConn() {
 		try {
 			conn.close();
 			System.out.println("Connection Closed");
+			return null;
 		}catch(SQLException e) {
 			System.out.println("Closing Connection...");
 			e.printStackTrace();
+			return null;
 		}
 	}
 }
